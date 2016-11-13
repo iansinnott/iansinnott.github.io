a7ae300eac50a2184983cbf413df08f602a32788
Ian Sinnott <ian@iansinnott.com>
2016-11-13 13:05:15 -0800

    Improve slide out menu

--- source/about/index.md
+++
@@ -1,6 +0,0 @@
----
-title: about
-date: 2016-11-12 17:10:16
----
-
-What do I do with this?
--- themes/parlance/source/js/main.js
+++ themes/parlance/source/js/main.js
@@ -8,6 +8,18 @@
   // element that captured the event, but I chose to use current target to avoid
   // using context.
   var handlers = {
+    openNav: function(e) {
+      var el = e.currentTarget;
+      var targetEl = $(el.dataset.target)[0];
+      targetEl.classList.add('open');
+    },
+
+    closeNav: function(e) {
+      var el = e.currentTarget;
+      var targetEl = $(el.dataset.target)[0];
+      targetEl.classList.remove('open');
+    },
+
     toggleNav: function(e) {
       var el = e.currentTarget;
       var targetEl = $(el.dataset.target)[0];
@@ -19,10 +31,17 @@
   var elements = $('[data-event-handler]');
 
   elements.forEach(function(el) {
-    var _ref = el.dataset.eventHandler.split(':')
-    var event = _ref[0];
-    var handler = _ref[1];
+    el.dataset.eventHandler.split(',').forEach(function(pair) {
+      var _ref = pair.split(':')
+      var event = _ref[0];
+      var handlerName = _ref[1];
+      var handler = handlers[handlerName];
 
-    el.addEventListener(event, handlers[handler]);
+      if (!handler) {
+        console.warn('Tried to assign undefined "' + handlerName + '" handler to "' + event + '" event.');
+      }
+
+      el.addEventListener(event, handler);
+    })
   });
 })();
