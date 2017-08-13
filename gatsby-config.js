c4dc51cc76ff4d2335558eff779c5bf54205e5f6
Ian Sinnott <ian@iansinnott.com>
2017-08-13 14:55:28 -0700

    Read content from filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,56 @@
+const rupture = require('rupture');
+const path = require('path');
+
+/**
+ * NOTE: options.name in the source filesystem plugin translates to
+ * `sourceInstanceName` in the graphql query
+ *
+ * Example query to get only 'posts':
+ *
+ *  {
+ *    files: allFile(limit: 3, sort: {fields: name, order: DESC}, filter: {sourceInstanceName: {eq: "posts"}}) {
+ *      edges {
+ *        node {
+ *          id
+ *          name
+ *          sourceInstanceName
+ *        }
+ *      }
+ *    }
+ *  }
+ */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: 'Ian Sinnott',
   },
-  plugins: [`gatsby-plugin-react-helmet`],
-}
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts', // See NOTE
+        path: path.resolve('./content/_posts'),
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'drafts',
+        path: path.resolve('./content/_drafts'),
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-stylus',
+      options: {
+        use: [rupture()],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-52511246-1',
+      },
+    },
+  ],
+};
+
