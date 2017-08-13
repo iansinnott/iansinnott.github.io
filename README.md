ef5b53630222f99e33fe8d35e7bf0d820d9b9d84
Ian Sinnott <ian@iansinnott.com>
2017-08-12 17:47:13 -0700

    Copy over initial gatsby files

---
+++ README.md
@@ -0,0 +1,11 @@
+# My Blog
+
+* Dev: `yarn start`
+* Build: `yarn build`
+* Deploy to live blog: `yarn deploy` (TODO)
+
+---
+
+TODO:
+
+* Need a more automated way to create new posts...
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const IndexPage = () =>
+  <div>
+    <h1>Hi people</h1>
+    <p>This is the initial state of the new site running on Gatsby</p>
+  </div>;
+
+export default IndexPage;
