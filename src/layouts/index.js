e71632182886d14830a0d8093f27ce9af8bd4736
Ian Sinnott <ian@iansinnott.com>
2017-08-14 08:40:46 -0700

    Reformat things

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,42 +1,40 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
+import Helmet from 'react-helmet';
 
-import './index.css'
+import './index.css';
 
-const Header = () =>
+const Header = () => (
   <div
     style={{
       background: 'rebeccapurple',
       marginBottom: '1.45rem',
-    }}
-  >
+    }}>
     <div
       style={{
         margin: '0 auto',
         maxWidth: 960,
         padding: '1.45rem 1.0875rem',
-      }}
-    >
+      }}>
       <h1 style={{ margin: 0 }}>
         <Link
-          to="/"
+          to='/'
           style={{
             color: 'white',
             textDecoration: 'none',
-          }}
-        >
+          }}>
           Gatsby
         </Link>
       </h1>
     </div>
   </div>
+);
 
-const TemplateWrapper = ({ children }) =>
+const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title='Gatsby Default Starter'
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
@@ -49,14 +47,14 @@ const TemplateWrapper = ({ children }) =>
         maxWidth: 960,
         padding: '0px 1.0875rem 1.45rem',
         paddingTop: 0,
-      }}
-    >
+      }}>
       {children()}
     </div>
   </div>
+);
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
-}
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
