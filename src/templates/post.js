a00147f761644bb5eae145794737b51520a1872c
Ian Sinnott <ian@iansinnott.com>
2017-08-27 14:56:54 -0700

    Fix disqus again

--- src/templates/post.js
+++ src/templates/post.js
@@ -30,7 +30,7 @@ const Comments = ({ disqusId, pageURL }) => {
   };
   (function() { // DON'T EDIT BELOW THIS LINE
     var d = document, s = d.createElement('script');
-    s.src = 'https://EXAMPLE.disqus.com/embed.js';
+    s.src = 'https://iansinnott.disqus.com/embed.js';
     s.setAttribute('data-timestamp', +new Date());
     (d.head || d.body).appendChild(s);
   })();
