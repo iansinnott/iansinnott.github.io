f543ddb42a8fc87b741bd881b99590fced2ddf84
Ian Sinnott <ian@iansinnott.com>
2017-08-27 14:33:30 -0700

    Change disqus rendering strategy

--- src/templates/post.js
+++ src/templates/post.js
@@ -22,24 +22,24 @@ const getContext = path(['pathContext']);
 
 const Comments = ({ disqusId, pageURL }) => {
   const disqus = `
-var disqus_config = function () {
-  ${pageURL ? `this.page.url = "${pageURL}"` : ''}
-  ${disqusId ? `this.page.identifier = "${disqusId}"` : ''}
-};
-*/
-(function() { // DON'T EDIT BELOW THIS LINE
-  var d = document, s = d.createElement('script');
-  s.src = 'https://EXAMPLE.disqus.com/embed.js';
-  s.setAttribute('data-timestamp', +new Date());
-  (d.head || d.body).appendChild(s);
-})();
+<div id='disqus_thread'></div>
+<script>
+  var disqus_config = function () {
+    ${pageURL ? `this.page.url = "${pageURL}"` : ''}
+    ${disqusId ? `this.page.identifier = "${disqusId}"` : ''}
+  };
+  */
+  (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://EXAMPLE.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+  })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 `.trim();
   return (
-    <div className={cx('Comments')}>
-      <div id='disqus_thread' />
-      <script dangerouslySetInnerHTML={{ __html: disqus }} />
-      <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-    </div>
+    <div className={cx('Comments')} dangerouslySetInnerHTML={{ __html: disqus }} />
   );
 };
 
